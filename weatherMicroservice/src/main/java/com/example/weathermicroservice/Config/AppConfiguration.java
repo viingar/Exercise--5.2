a1f0fbe0bad116308d9ee7fb5005ed5fba9233a0
viingar <myuniverse8082@gmail.com>
2024-07-10 18:29:26 +0300

    Exercise 5.4 (Кэширование данных)

---
+++ weatherMicroservice/src/main/java/com/example/weathermicroservice/Config/AppConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.weathermicroservice.Config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableScheduling
+@EnableCaching
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    public ConcurrentMapCacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("weather");
+    }
+}
--- weatherMicroservice/src/main/java/com/example/weathermicroservice/Controller/WeatherController.java
+++ weatherMicroservice/src/main/java/com/example/weathermicroservice/Controller/WeatherController.java
@@ -1,10 +1,19 @@
 package com.example.weathermicroservice.Controller;
 
+import com.example.weathermicroservice.Model.Main;
 import com.example.weathermicroservice.Model.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 @RestController
 public class WeatherController {
@@ -13,9 +22,30 @@ public class WeatherController {
     private RestTemplate restTemplate;
     @Value("${appid}")
     private String appId;
+    @Value("${url.weather}")
+    private String urlWeather;
+    @Autowired
+    private CacheManager cacheManager;
 
-    @GetMapping("/weather")
-    public Root getWeather() {
-        return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?lat=54.1838&lon=45.1749&units=metric&appid=" + appId, Root.class);
+    @GetMapping("/cache")
+    public Map<Object, Object> getCache() {
+        ConcurrentMapCache cache = (ConcurrentMapCache) cacheManager.getCache("weather");
+        if (cache != null) {
+            ConcurrentMap<Object, Object> map = cache.getNativeCache();
+            return map;
+        } else {
+            return Map.of();
+        }
+    }
+    @Cacheable(value = "weather", key = "#lat.concat(':').concat(#lon)")
+    @GetMapping
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon) {
+        String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s",
+                urlWeather, lat, lon, appId);
+        return restTemplate.getForObject(request, Root.class).getMain();
+    }
+    @Scheduled(fixedRate = 60000)
+    @CacheEvict(value = "weather", allEntries = true)
+    public void cacheEvict() {
     }
 }
